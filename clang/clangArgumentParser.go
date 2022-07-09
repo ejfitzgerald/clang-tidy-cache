dfe2bcf7f829cbbe8f0cc571daa311050b17a161
Ben Boeckel <mathstuf@gmail.com>
2022-07-09 10:12:41 -0400

    clangArgumentParser: use `shlex` to parse commands
    Commands in the JSON database may be escaped for the shell. Perform
    unescaping so that escaped characters don't get mangled.

--- clang/clangArgumentParser.go
+++ clang/clangArgumentParser.go
@@ -3,11 +3,11 @@ package clang
 import (
 	"crypto/sha256"
 	"errors"
+	"github.com/google/shlex"
 	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 type CompilerCommand struct {
@@ -18,7 +18,10 @@ type CompilerCommand struct {
 }
 
 func ParseClangCommandString(commands string) (*CompilerCommand, error) {
-	words := strings.Fields(commands)
+	words, err := shlex.Split(commands)
+	if err != nil {
+		return nil, err
+	}
 
 	var cmd CompilerCommand
 	cmd.Compiler = words[0]
